12bf435aae6e00e97fb94bd2d34be6cedb7c040f
Leonardo Donelli <learts92@gmail.com>
2018-03-19 14:53:13 +0100

    fish: add ssh alias to set hostname in window title

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -8,3 +8,21 @@ function ..... ; cd ../../../.. ; end
 
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias heaviest-directories="du -h --max-depth 1 2>/dev/null | sort -h -r | head -n 20"
+
+# When connecting to ssh, put hostname in window title
+function ssh
+	# for loop to find the first positional argument
+	# which is probably the hostname
+	set flag "";
+	for option in $argv
+		if string match -- "-*" $option
+			set flag "true";
+		else if test -z $flag
+			echo -ne "\033]0;$option\007";
+			break;
+		else
+			set flag "";
+		end
+	end
+	/usr/bin/ssh $argv;
+end
