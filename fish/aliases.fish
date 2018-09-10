0feb576f4e2121c746691a638f080e9f81323d03
Leonardo Donelli <learts92@gmail.com>
2018-09-10 15:24:51 +0200

    A 2-letters command is fast to type, but have you tried 1?

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -8,6 +8,7 @@ function ..... ; cd ../../../.. ; end
 
 alias myip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias heaviest-directories="du -h --max-depth 1 2>/dev/null | sort -h -r | head -n 20"
+alias l="ls -alh"
 
 # When connecting to ssh, put hostname in window title
 function ssh
